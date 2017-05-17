47dd6dbcde50f69d4c6a42ce36553c5c006d32d8
matheuslbarros <matheuslbarros@gmail.com>
2017-05-17 11:43:57 -0300

    Definição da url do mongodb

--- server.js
+++ server.js
@@ -4,21 +4,8 @@ var serverPort = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-	var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-		mongoHost        = process.env[mongoServiceName + '_SERVICE_HOST'],
-		mongoPort        = process.env[mongoServiceName + '_SERVICE_PORT'],
-		mongoDatabase    = process.env[mongoServiceName + '_DATABASE'],
-		mongoPassword    = process.env[mongoServiceName + '_PASSWORD']
-		mongoUser        = process.env[mongoServiceName + '_USER'];
-	
-	if (mongoHost && mongoPort && mongoDatabase) {
-		mongoURL = 'mongodb://';
-		if (mongoUser && mongoPassword) {
-			mongoURL += mongoUser + ':' + mongoPassword + '@';
-		}
-		mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-	}
+if (mongoURL == null) {
+	throw Error("Requires mongodb enviroment config");
 }
 
 var express = require('express');
