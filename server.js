42f182842c71d4bd9d28c61f0187522652d6b8f5
matheuslbarros <matheuslbarros@gmail.com>
2017-05-17 10:55:05 -0300

    Database settings

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 
-var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080
+var serverIp = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var serverPort = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-var mongodb_database_name = "sampledb";
-var mongodb_connection_string = (process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://127.0.0.1:27017/') + mongodb_database_name;
+var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+	var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+		mongoHost        = process.env[mongoServiceName + '_SERVICE_HOST'],
+		mongoPort        = process.env[mongoServiceName + '_SERVICE_PORT'],
+		mongoDatabase    = process.env[mongoServiceName + '_DATABASE'],
+		mongoPassword    = process.env[mongoServiceName + '_PASSWORD']
+		mongoUser        = process.env[mongoServiceName + '_USER'];
+	
+	if (mongoHost && mongoPort && mongoDatabase) {
+		mongoURL = 'mongodb://';
+		if (mongoUser && mongoPassword) {
+			mongoURL += mongoUser + ':' + mongoPassword + '@';
+		}
+		mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+	}
+}
 
 var express = require('express');
 var mongoose = require('mongoose');
@@ -13,7 +29,7 @@ var Note = require('./api/models/Note');
 var NoteRoute = require('./api/routes/Note');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(mongodb_connection_string);
+mongoose.connect(mongoURL);
 
 var app = express();
 
@@ -22,10 +38,16 @@ app.use(bodyParser.json());
 
 NoteRoute(app);
 
-app.use(function(req, res) {
-	res.status(404).send({url: req.originalUrl + ' not found'})
+app.get('/', function (req, res) {
+	res.send('GET request to the homepage');
 });
 
-app.listen(server_port, server_ip, function() {
-	console.log("Listening on " + server_ip + ", port " + server_port);
+app.use(function(req, res) {
+	res.status(404).send({url: req.originalUrl + ' not found'});
 });
+
+app.listen(serverPort, serverIp, function() {
+	console.log("Listening on " + serverIp + ", port " + serverPort);
+});
+
+module.exports = app;
